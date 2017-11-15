0f2e5be8f070abfc657fbd2f78b82d7921546397
digvice <digvice81@gmail.com>
2017-11-15 20:03:16 +0200

    Fixed library

--- library.php
+++ library.php
@@ -6,15 +6,11 @@
 
     <main role="main">
 <!-- //library// -->
-
-<article>
+<div class="cont">
+<article class="main">
     <section class="wrapper margin-bottom">
-
         <h1>Библиотека</h1>
-
-        <div class="library">
-
-
+            <div class="library">
             <?php
             global $post;
             $args = array('posts_per_page' => 100,'post_type' => 'library', 'order' => 'ASC' );
@@ -23,26 +19,20 @@
                 ?>
                 <div class="library-wrapper">
                     <div class="library_img" >
-                        <?php the_post_thumbnail(); ?>
+                        <img class="library_img_img" src=" <?php echo get_the_post_thumbnail_url($post->ID, 'medium'); ?> ">
                     </div>
 
                     <h4><?php echo get_post_meta( $post->ID, 'level', true ); ?></h4>
-                    <div class="library_annotation"><?php the_content(); ?></div>
+                    <p class="library_annotation"><?php the_content(); ?></p>
                     <div style="height: 100%"></div>
                     <div>
                         
                         <?php
-
                         $file = get_field('book');
-
                         if( $file ) {
-
                             $url = wp_get_attachment_url( $file );
-
                             ?><a target="blank" class="library_button-download" href="<?php echo $url; ?>" >Скачать</a><?php
-
                         }
-
                         ?>
                     </div>
                 </div>
@@ -50,11 +40,8 @@
             }
             wp_reset_postdata();
             ?>
-
-
         </div>
     </section>
 </article>
-
-    </main>
-    <?php get_footer(); ?>
+</main>
+<?php get_footer(); ?>

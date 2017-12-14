d2216c26cf130b3ae995954578e182fdd419d611
Anna <vovchenko.anna@dizboard.com>
2017-12-14 10:28:13 +0200

    added new files

---
+++ partners.php
@@ -0,0 +1,19 @@
+<?php 
+     /*
+    Template Name: Partners
+    */
+    get_header(); ?>
+
+<main role="main">
+
+<header class="partners_img-container">
+    <img src="<?php echo get_template_directory_uri() ?>/img/partners.png" alt="partners page">
+    <div class="partners-title-container">
+        <h1 class="partners-title">
+            Наши партнёры
+        </h1>
+    </div> 
+</header>
+
+</main>
+<?php get_footer(); ?>

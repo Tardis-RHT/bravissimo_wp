d3086befc0028568172a7dbce903ab42153a7917
Anna <vovchenko.anna@dizboard.com>
2017-12-14 17:21:55 +0200

    changed clients

--- partners.php
+++ partners.php
@@ -10,7 +10,7 @@
     <img src="<?php echo get_template_directory_uri() ?>/img/partners.png" alt="partners page">
     <div class="partners-title-container">
         <h1 class="partners-title">
-            Наши партнёры
+            Наши клиенты
         </h1>
     </div> 
 </header>
@@ -18,7 +18,7 @@
     <div class="single-partner">
         <img src="<?php echo get_template_directory_uri() ?>/img/italy-tips1.png" alt="partner1">
         <div class="single-partner-info">
-            <h3 class="single-partner-name">Partner 1</h3>
+            <h3 class="single-partner-name">Клиент 1</h3>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus rerum dolorum dolores aliquid suscipit maiores culpa, magnam similique laudantium repudiandae facere at! Necessitatibus maxime beatae debitis minus culpa, recusandae quos!</p>
             <a href="#" class="button">link</a>
         </div>

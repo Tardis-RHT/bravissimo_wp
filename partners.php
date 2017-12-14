d1237133671735acb3feda8de5c76ec2f05e6285
Anna <vovchenko.anna@dizboard.com>
2017-12-14 16:29:10 +0200

    working with partners

--- partners.php
+++ partners.php
@@ -14,6 +14,15 @@
         </h1>
     </div> 
 </header>
-
+<section class="partners-container wrapper">
+    <div class="single-partner">
+        <img src="<?php echo get_template_directory_uri() ?>/img/italy-tips1.png" alt="partner1">
+        <div class="single-partner-info">
+            <h3 class="single-partner-name">Partner 1</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus rerum dolorum dolores aliquid suscipit maiores culpa, magnam similique laudantium repudiandae facere at! Necessitatibus maxime beatae debitis minus culpa, recusandae quos!</p>
+            <a href="#" class="button">link</a>
+        </div>
+    </div>
+</section>
 </main>
 <?php get_footer(); ?>
